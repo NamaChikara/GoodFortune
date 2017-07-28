706a962cb07c39ca04657ef6179858e02ff211c0
Zachary Barry <zackbarry13@gmail.com>
2017-07-28 16:27:42 -0700

    codepen files

---
+++ index.js
@@ -0,0 +1,5 @@
+$("#promo-img1, #promo-img2, #promo-img3").find("div").addClass("col-xs-12 col-sm-4");
+$("promo-ing").find("img").addClass("img-responsive center-block promo");
+$("h3, h5, h6").addClass("text-center");
+$("button").addClass("float-left");
+$("span").addClass("text-center");
